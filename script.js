1a4ba3b57348045d0a585f67b5dd9819e2c135c3
Adarsh23100 <adarshrajesh23100@gmail.com>
2022-02-21 20:44:28 +0530

    new commit

--- script.js
+++ script.js
@@ -9,3 +9,66 @@ function addItem(event) {
 
     text.value="";
 }
+
+function getItems() {
+  db.collection("todo-items").onSnapshot((snapshot) => {
+    let items = [];
+    snapshot.docs.forEach((doc) => {
+      items.push({
+        id: doc.id,
+        ...doc.data()
+      })
+    })
+    generateItems(items);
+  })
+}
+
+function generateItems(items) {
+  let itemsHtml = "";
+  
+  items.forEach((item) => {
+    itemsHtml += `
+    <div class="todo-item">
+        <div class="check">
+            <div data-id=${item.id} class="check-mark ${item.status == "completed" ? "checked" : ""}">
+                <img src="./assets/icon-check.svg" alt="">
+            </div>
+        </div>
+        <div class="todo-text ${item.status == "completed" ? "checked" : ""}">
+            ${item.text}
+        </div>
+    </div>
+    `
+  })
+  document.querySelector(".todo-items").innerHTML = itemsHtml;
+  createEventListeners();
+}
+
+function createEventListeners(){
+  let todoCheckMarks = document.querySelectorAll(".todo-item .check-mark");
+  todoCheckMarks.forEach((checkMark) => {
+    checkMark.addEventListener("click", function() {
+      markCompleted(checkMark.dataset.id);
+    })
+  })
+}
+
+function markCompleted(id) {
+  let item = db.collection("todo-items").doc(id);
+  item.get().then(function(doc){
+    if(doc.exists){
+      let status = doc.data().status;
+      if(status == "active"){
+        item.update({
+          status: "completed"
+        })
+      }else if(status == "completed"){
+        item.update({
+          status: "active"
+        })
+      }
+    }
+  })
+}
+
+getItems();
